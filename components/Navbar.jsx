bffdd87acf7e09cc126f0c527e1c94e26d97db2c
mrelashvili <mrmrela5@gmail.com>
2022-11-06 02:46:51 +0400

    implement cart component & update global state context

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import Link from 'next/link'
 import { AiOutlineShopping } from 'react-icons/ai'
 
+import { Cart } from './';
+
+import { useStateContext } from '../context/stateContext';
+
 const Navbar = () => {
+
+  const { showCart, setShowCart, totalQuantities } = useStateContext()
   return (
     <div className="navbar-container">
       <p className="logo">
@@ -11,10 +17,12 @@ const Navbar = () => {
         </Link>
       </p>
 
-      <button type='button' className="cart-icon">
+      <button type='button' className="cart-icon" onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
-        <span className="cart-item-qty">2</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
+
+      {showCart && <Cart />}
     </div>
   )
 }
--- context/stateContext.js
+++ context/stateContext.js
@@ -11,6 +11,9 @@ export const StateContext = ( { children } ) => {
   const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
 
+  let foundProduct;
+  let index;
+
 
   const onAdd = (product, quantity) => {
     const checkProductInCart = cartItems.find((item) => item._id === product._id);
@@ -36,6 +39,29 @@ export const StateContext = ( { children } ) => {
 
   } 
 
+  const toggleCartItemQuantity = ({ id, value }) => {
+    foundProduct = cartItems.find(item => item._id === id)
+    index = cartItems.findIndex(item => item._id === id)
+
+    if(value === 'inc')  {
+      let newCartItems = [...cartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 }]
+
+      setCartItems(newCartItems)
+      setTotalPrice(prev => prev + foundProduct.price)
+      setTotalQuantities(prev => prev + 1)
+
+    } else if (value === 'dec') {
+      
+      if (foundProduct.quantity > 1) {
+        let newCartItems = [...cartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 }]
+
+        setCartItems(newCartItems)
+        setTotalPrice(prev => prev - foundProduct.price)
+        setTotalQuantities(prev => prev - 1)
+      }
+    }
+  }
+
   //// For increase quantity
   const incQty = () => {
     setQty((prev) => prev + 1)
@@ -53,13 +79,15 @@ export const StateContext = ( { children } ) => {
     <Context.Provider 
     value={{
       showCart, 
+      setShowCart,
       cartItems, 
       totalPrice, 
       totalQuantities, 
       qty,
       incQty, 
       decQty, 
-      onAdd
+      onAdd,
+      toggleCartItemQuantity
     }}>
       {children}
     </Context.Provider>
